88e6507f2446590fdc5458d4a4e6887275183cdf
Thinh Van <thinh.vovan@hcmut.edu.vn>
2025-04-06 23:41:37 +0700

    queue + sched

--- src/sched.c
+++ src/sched.c
@@ -31,11 +31,14 @@ void init_scheduler(void) {
 
 	for (i = 0; i < MAX_PRIO; i ++) {
 		mlq_ready_queue[i].size = 0;
+		mlq_ready_queue[i].front = mlq_ready_queue[i].rear = 0; // Added by me!
 		slot[i] = MAX_PRIO - i; 
 	}
 #endif
 	ready_queue.size = 0;
+	ready_queue.front = ready_queue.rear = 0; // Added by me!
 	run_queue.size = 0;
+	run_queue.front = run_queue.rear = 0; // Added by me!
 	pthread_mutex_init(&queue_lock, NULL);
 }
 
@@ -51,6 +54,14 @@ struct pcb_t * get_mlq_proc(void) {
 	/*TODO: get a process from PRIORITY [ready_queue].
 	 * Remember to use lock to protect the queue.
 	 * */
+	pthread_mutex_lock(&queue_lock);
+	for (int i = 0; i < MAX_PRIO; ++i) {
+		if (mlq_ready_queue[i].size > 0 ){
+			proc = dequeue(&mlq_ready_queue[i]);
+			break;
+		}
+	}
+	pthread_mutex_unlock(&queue_lock);
 	return proc;	
 }
 
@@ -76,7 +87,7 @@ void put_proc(struct pcb_t * proc) {
 	proc->running_list = & running_list;
 
 	/* TODO: put running proc to running_list */
-
+	enqueue(&running_list, proc);
 
 	return put_mlq_proc(proc);
 }
@@ -87,7 +98,8 @@ void add_proc(struct pcb_t * proc) {
 	proc->running_list = & running_list;
 
 	/* TODO: put running proc to running_list */
-
+	enqueue(&running_list, proc);
+	
 	return add_mlq_proc(proc);
 }
 #else
@@ -96,6 +108,9 @@ struct pcb_t * get_proc(void) {
 	/*TODO: get a process from [ready_queue].
 	 * Remember to use lock to protect the queue.
 	 * */
+	pthread_mutex_lock(&queue_lock);
+	dequeue(&ready_queue);
+	pthread_mutex_unlock(&queue_lock);
 	return proc;
 }
 
@@ -104,6 +119,7 @@ void put_proc(struct pcb_t * proc) {
 	proc->running_list = & running_list;
 
 	/* TODO: put running proc to running_list */
+	enqueue(&running_list, proc);
 
 	pthread_mutex_lock(&queue_lock);
 	enqueue(&run_queue, proc);
@@ -115,7 +131,7 @@ void add_proc(struct pcb_t * proc) {
 	proc->running_list = & running_list;
 
 	/* TODO: put running proc to running_list */
-
+	enqueue(&running_list, proc);
 	pthread_mutex_lock(&queue_lock);
 	enqueue(&ready_queue, proc);
 	pthread_mutex_unlock(&queue_lock);	
